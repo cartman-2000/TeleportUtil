cf25f212a22bd2e32b1d754267dcffb5bbfbc82f
cartman-2000 <cartman-2000@cartman-2000.net>
2015-06-27 16:58:58 -0700

    Initial commit.

---
+++ CommandLocate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Rocket.Unturned.Commands;
+using Rocket.Unturned.Player;
+using Rocket.Unturned;
+
+
+namespace TeleportUtil
+{
+    class CommandLocate : IRocketCommand
+    {
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+
+        public void Execute(RocketPlayer caller, string[] command)
+        {
+            if (caller == null)
+            {
+                RocketChat.Say(caller, "Can't locate player.");
+                return;
+            }
+            RocketChat.Say(caller, TeleportUtil.Instance.Translate("location_on_map", new object[] { Math.Round(caller.Position.x, 2), Math.Round(caller.Position.y, 2), Math.Round(caller.Position.z, 2)}));
+        }
+
+        public string Help
+        {
+            get { return "Prints out the location of the player on the map for usage in the /tp command."; }
+        }
+
+        public string Name
+        {
+            get { return "locate"; }
+        }
+
+        public bool RunFromConsole
+        {
+            get { return false; }
+        }
+
+        public string Syntax
+        {
+            get { return ""; }
+        }
+    }
+}
---
+++ TeleportUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Rocket.Unturned.Plugins;
+using System.Collections.Generic;
+
+namespace TeleportUtil
+{
+    public class TeleportUtil : RocketPlugin
+    {
+        public static TeleportUtil Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+        }
+
+        public override Dictionary<string, string> DefaultTranslations
+        {
+            get
+            {
+                return new Dictionary<string, string>
+                {
+                    {
+                        "location_on_map",
+                        "Your location on the map is: x:{0}, y:{1}, z:{2}"
+                    }
+                };
+            }
+        }
+    }
+}
